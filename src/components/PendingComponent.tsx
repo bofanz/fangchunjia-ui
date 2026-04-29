9b150acdb699bc2d8ca7be787a3571e1e1ba07f4
Bo Zhang <bofanz2000@gmail.com>
2026-04-29 02:49:44 +0200

    FANG-20: refactored page layouts;

---
+++ src/components/PendingComponent.tsx
@@ -0,0 +1,7 @@
+export function PendingComponent() {
+  return (
+    <section className="pt-[var(--section-top-spacing)]">
+      <div className="content">Loading...</div>
+    </section>
+  );
+}
--- src/routes/_layout.projects.$projectId.tsx
+++ src/routes/_layout.projects.$projectId.tsx
@@ -1,7 +1,6 @@
 import MediaGrid from '@/components/MediaGrid';
 import { createFileRoute, getRouteApi } from '@tanstack/react-router';
 import { fetchProject, type QueryContext } from '@/utils/queries';
-import Layer from '@/components/Layer';
 import TiptapRenderer from '@/components/TiptapRenderer';
 import { parseJsonContent } from '@/components/admin/Tiptap/parseJsonContent';
 
@@ -23,52 +22,18 @@ export const Route = createFileRoute('/_layout/projects/$projectId')({
 });
 
 function PendingComponent() {
-  return (
-    <>
-      <Layer>
-        <div className="overflow-y-auto h-full [scrollbar-width:none]">
-          <div className="min-h-screen flex justify-start pt-24">
-            <div>
-              <div className="pb-4">Fetching project...</div>
-            </div>
-          </div>
-        </div>
-      </Layer>
-    </>
-  );
+  return <div className="project">Fetching project...</div>;
 }
 
 function NotFoundComponent() {
-  return (
-    <>
-      <Layer>
-        <div className="overflow-y-auto h-full [scrollbar-width:none]">
-          <div className="min-h-screen flex justify-start pt-24">
-            <div>
-              <div className="pt-16 pb-4">Project not found</div>
-            </div>
-          </div>
-        </div>
-      </Layer>
-    </>
-  );
+  return <div className="project">Project not found</div>;
 }
 
 function ErrorComponent({ error }: { error: Error }) {
   return (
-    <>
-      <Layer>
-        <div className="overflow-y-auto h-full [scrollbar-width:none]">
-          <div className="min-h-screen flex justify-start pt-24">
-            <div>
-              <div className="pt-16 pb-4">
-                An error occurred when fetching the project: {error.message}
-              </div>
-            </div>
-          </div>
-        </div>
-      </Layer>
-    </>
+    <div className="project">
+      An error occurred when fetching the project: {error.message}
+    </div>
   );
 }
 
@@ -76,30 +41,20 @@ function RouteComponent() {
   const routeApi = getRouteApi('/_layout/projects/$projectId');
   const project = routeApi.useLoaderData();
   return (
-    <Layer key={project.id}>
-      <div className="overflow-y-auto h-full [scrollbar-width:none]">
-        <div className="min-h-screen flex justify-start pt-24">
-          <div className="flex flex-col p-4 w-full sm:p-8 sm:w-4/5">
-            <div className="">
-              <h1 className="text-xl font-bold">{project.name}</h1>
-              <h2 className="text-lg">{project.year}</h2>
-              <div>{project.link}</div>
-              <div>
-                <TiptapRenderer
-                  content={parseJsonContent(project.description)}
-                />
-              </div>
-            </div>
-
-            <div className="pt-4 pb-8">
-              <MediaGrid
-                media={project.media}
-                mediaLayout={project.mediaLayout}
-              />
-            </div>
+    <div className="content project">
+      <div className="">
+        <div>
+          <h1 className="text-xl font-bold">{project.name}</h1>
+          <h2 className="text-lg">{project.year}</h2>
+          <div>{project.link}</div>
+          <div>
+            <TiptapRenderer content={parseJsonContent(project.description)} />
           </div>
         </div>
+        <div className="pt-4 pb-8">
+          <MediaGrid media={project.media} mediaLayout={project.mediaLayout} />
+        </div>
       </div>
-    </Layer>
+    </div>
   );
 }
